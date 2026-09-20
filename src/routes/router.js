7d6d99fde0edbd1319787ecee4b98cec1b927993
Marco Sandoval <msandoval2@byupathway.edu>
2026-09-20 12:52:55 -0700

    Change API routes to use root path
    Updated router.js to be in sync with the current main branch

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,11 +2,9 @@ import { Router } from "express";
 import apiRoutes from "./api-routes.js";
 import ejsRoutes from "./ejs-routes.js";
 
-
-
 const router = Router();
 
 router.use("/", ejsRoutes);
-router.use("/api", apiRoutes);
+router.use("/", apiRoutes);
 
 export default router;
